caaf20e5e4977a49e2c3ac9147127a8d93fc6c76
leon <leon00albert@gmail.com>
2023-05-28 19:38:29 +0300

    feat: add snippet to file tree

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 use Artemis\Core\Router\Router;
 use Artemis\Core\DataBases\DB;
+
 $app = Router::getInstance();
-$snippets = DB::new("JSON","snippets");
+$snippets = DB::new("JSON", "snippets");
 
 $app->get("/", function ($req, $res) {
     $res->render(__DIR__ . "/views/index.php");
@@ -19,42 +20,54 @@ $app->get("/api/snippets", function ($req, $res) {
     $res->status(201);
 });
 
+function readDatabase()
+{
+    $data = file_get_contents('database.json');
+    return json_decode($data, true);
+}
+
+// Function to write data to the JSON file
+function writeDatabase($data)
+{
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents('database.json', $json);
+}
 
 $app->get("/api/filetrees", function ($req, $res) {
-    global $snippets;
-    $arr = [
-        [
-            'text' => 'Root',
-            'children' => [
-                [
-                    'text' => 'Beverages',
-                    'children' => [
-                        'Water',
-                        'Coffee',
-                        [
-                            'text' => 'Tea',
-                            'children' => [
-                                'Black Tea',
-                                'White Tea',
-                                [
-                                    'text' => 'Green Tea',
-                                    'children' => ['Sencha', 'Gyokuro', 'Matcha', 'Pi Lo Chun']
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
-    ];
-    $res->json($arr);
+
+    $res->json(readDatabase());
+    $res->status(201);
+});
+
+$app->post("/api/filetrees", function ($req, $res) {
+    $db = readDatabase();
+    array_push($db, ["text" => $req->body()["name"], "children" => [], "id" => uniqid()]);
+    writeDatabase($db);
     $res->status(201);
 });
 
 $app->post("/api/snippets", function ($req, $res) {
+
+    $db = readDatabase();
+    $key = array_search($req->body()["id"], array_column($db, 'id'));
+
+    if ($key !== false) {
+        array_push(
+            $db[$key]["children"],
+            [
+                "text" => $req->body()["title"],
+                "id" => uniqid(),
+                "code" => $req->body()["code"],
+                "language" => $req->body()["language"]
+            ]
+        );
+        writeDatabase($db);
+
+        $res->status(201);
+    } else {
+        $res->status(500);
+    }
     global $snippets;
-    $snippets->create($req->body());
-    $res->status(201);
 });
 
 
@@ -70,8 +83,6 @@ $app->get("/public/:file", function ($req, $res) {
 $app->get("*", function ($req, $res) {
     $res->send("404");
     $res->status(404);
-
-   
 });
 
 $app->listen("/", function () {
--- views/index.php
+++ views/index.php
@@ -53,15 +53,12 @@
                                 var snippetElement = document.createElement("div");
                                 snippetElement.className = "card";
                                 snippetElement.innerHTML = `
-                        
-                    
                                         <pre>
                                             <code class="language-js}">
                                             ${res.code}
                                             </code>
                                         </pre>
-                        
-                
+
                             `;
                                 snippetsDiv.appendChild(snippetElement);
                             });
@@ -92,6 +89,7 @@
                 fetch('/api/filetrees')
                     .then(response => response.json())
                     .then(res => {
+                        console.log(res)
                         const treeView = createTreeView(res);
                         const container = document.getElementById("filetree");
                         container.appendChild(treeView);
@@ -101,51 +99,93 @@
             };
 
 
-            function createTreeView(data) {
+            function createTreeView(data, i, item) {
+                console.log(data)
                 const ul = document.createElement('ul');
+                const add = document.createElement('p');
+                if (i) {
+                    add.className = 'add-folder';
+                    add.textContent = "add snippet"
+                    add.addEventListener("click", function() {
+                        window.location = '/?id=' + item.id;
+                    });
+                }else{
+                    const inputElement = document.createElement('input');
+                    inputElement.name = "name";
+                    inputElement.id = "inputElement";
+                    add.className = 'add-folder';
+                    add.textContent = "add folder"
+                    ul.appendChild(add);
+                    add.addEventListener("click", function() {
+                        let input = {
+                            name: document.getElementById("inputElement").value
+                        }
+                        fetch('/api/filetrees', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(input)
+                            })
+                            .then(response => {
+                                if (!response.ok) {
+                                    throw new Error('Network response was not ok');
+                                }
+
+                                return response.json();
+                            }).then(data => {
+
+                                window.location = '/';
+
+                            })
+                            .catch(error => {
+                                console.error('There was a problem with the fetch operation:', error);
+                            });
+                    });
+                }
+
+
+                ul.appendChild(add);
                 data.forEach((item, i) => {
-                    console.log(item)
+
                     const li = document.createElement('li');
-                    if (typeof item === 'object') {
+                    if (typeof item === 'object' && !item.code) {
                         const span = document.createElement('span');
                         span.className = 'caret';
                         span.textContent = item.text;
-
                         li.appendChild(span);
-                        span.addEventListener("click", function() {
-                        console.log("click")
-                        this.parentElement.querySelector(".nested").classList.toggle("active");
-                        this.classList.toggle("caret-down");
-                    });
 
-                        const nestedUl = createTreeView(item.children);
+                        span.addEventListener("click", function() {
+
+                            this.parentElement.querySelector(".nested").classList.toggle("active");
+                            this.classList.toggle("caret-down");
+                        });
+
+                        const nestedUl = createTreeView(item.children, true,item    );
                         nestedUl.className = "nested";
                         li.appendChild(nestedUl);
                     } else {
-                        li.textContent = item;
+                        li.textContent = item.text;
                     }
 
                     ul.appendChild(li);
                 });
 
-     
+
 
                 return ul;
             }
 
-
-
-
-
-
             function handle_add() {
+                const queryString = window.location.search;
+                const params = new URLSearchParams(queryString);
+                const id = params.get('id');
 
                 const input = {
-
                     code: document.getElementById('codeInput').value,
-                    langauge: "php",
-
-
+                    id: id,
+                    language: "php",
+                    title: "php snippet",
                 }
 
                 fetch('/api/snippets', {
