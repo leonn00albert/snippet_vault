b32786c69393482f61764e4c8ff8095dcc79149d
leon <leon00albert@gmail.com>
2023-05-31 13:33:35 +0300

    feat: add styling for file tree add font awesome

--- views/header.php
+++ views/header.php
@@ -9,6 +9,7 @@
         <link href="/public/prism.css" rel="stylesheet" />
         <link href="/public/bootstrap.min.css" rel="stylesheet" />
         <link href="/public/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>Document</title>
     </head>
 
