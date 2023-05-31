450f2eed06d320f97fa0d8abfbda9ad3054eb711
leon <leon00albert@gmail.com>
2023-05-31 17:31:53 +0300

    feat: add feed of new snippets

---
+++ utils.php
@@ -0,0 +1,22 @@
+    <?php
+
+    function getTimeAgo($timestamp)
+    {
+        $time = new DateTime($timestamp);
+        $now = new DateTime();
+        $diff = $now->diff($time);
+
+        if ($diff->y > 0) {
+            return $diff->y . ' year' . ($diff->y > 1 ? 's' : '') . ' ago';
+        } elseif ($diff->m > 0) {
+            return $diff->m . ' month' . ($diff->m > 1 ? 's' : '') . ' ago';
+        } elseif ($diff->d > 0) {
+            return $diff->d . ' day' . ($diff->d > 1 ? 's' : '') . ' ago';
+        } elseif ($diff->h > 0) {
+            return $diff->h . ' hour' . ($diff->h > 1 ? 's' : '') . ' ago';
+        } elseif ($diff->i > 0) {
+            return $diff->i . ' minute' . ($diff->i > 1 ? 's' : '') . ' ago';
+        } else {
+            return 'just now';
+        }
+    }
